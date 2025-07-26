596fa717cf14c5e8a7ebc9883054d86da83d2ef6
Alperen Aydın <alpaydin6177@gmail.com>
2025-07-26 17:29:45 +0300

    init

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+
+document.querySelector('#menu-acici-buton').addEventListener('click', (e) => {
+            document.querySelector('.acilir-yan-menu').classList.add('aktif');
+        })
+
+
+        
+        document.querySelector('#menu-kapatici-buton').addEventListener('click', (e) => {
+            document.querySelector('.acilir-yan-menu').classList.remove('aktif');
+        })
+
+
+
